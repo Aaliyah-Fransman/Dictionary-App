3ec78ee87bb17abee80b76ebab707730777e3878
Aaliyah-Fransman <aaliyah.fransman@gmail.com>
2025-12-08 00:05:27 +0200

    Updated my components and CSS

--- src/App.js
+++ src/App.js
@@ -1,32 +1,40 @@
+
 import "./App.css";
 import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Dictionary App</h1>
-      <main>
-        <Dictionary />
-      </main>
-      <footer>
-        <small>
-          This project was coded by{" "}
-          <a href="https://github.com/Aaliyah-Fransman" target="_blank">
-            Aaliyah Fransman
-          </a>{" "}
-          and is{" "}
-          <a
-            href="https://github.com/Aaliyah-Fransman/My-Weather-App-Project"
-            target="_blank"
-          >
-            on GitHub
-          </a>{" "}
-          and{" "}
-          <a href="https://theskycast.netlify.app/?#" target="_blank">
-            hosted on Netlify
-          </a>
-        </small>
-      </footer>
+      <div className="container">
+       
+      <header className="App-header">
+ <h1>Dictionary App</h1>
+</header>
+
+        <main>
+          <Dictionary defaultKeyword="sunset" />
+        </main>
+        <footer>
+          <small>
+            This project was coded by{" "}
+            <a href="https://github.com/Aaliyah-Fransman" target="_blank" rel="noopener noreferrer">
+              Aaliyah Fransman
+            </a>{" "}
+            and is{" "}
+            <a
+              href="https://github.com/Aaliyah-Fransman/My-Weather-App-Project"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              on GitHub
+            </a>{" "}
+            and{" "}
+            <a href="https://theskycast.netlify.app/?#" target="_blank" rel="noopener noreferrer">
+              hosted on Netlify
+            </a>
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
