a26d58a0f014fccbffcf3d74ecc6e3959f3b69c8
Aaliyah-Fransman <aaliyah.fransman@gmail.com>
2025-12-07 15:10:49 +0200

    Updated the CSS

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./styles.css";
+import "./App.css";
 import Dictionary from "./Dictionary";
 
 export default function App() {
--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,11 +1,13 @@
 import React from "react";
+import "./Phonetic.css";
 
 export default function Phonetic(props) {
-    console.log(props.phonetic);
-    return ( <div className="Phonetic">
-        <a href={props.phonetic.audio} target="_blank">Listen</a><br/>
-        {props.phonetic.text}
-
+  return (
+    <div className="Phonetic">
+      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+        Listen
+      </a>
+      <span className="text">{props.phonetic.text}</span>
     </div>
-    );
+  );
 }
